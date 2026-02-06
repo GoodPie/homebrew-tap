2fc74416cd102e3855365b857f7eeae9a51aabc7
Brandyn Britton <Brandynbb96@gmail.com>
2026-02-06 17:45:28 +0800

    fix: version in cask

--- Casks/buildkite-notifier.rb
+++ Casks/buildkite-notifier.rb
@@ -2,7 +2,7 @@ cask "buildkite-notifier" do
     version "0.0.4"
     sha256 "60ed0684a1cb34cffb43b35804d762970782ed8d0736b7ad3a1c191036912198"
 
-    url "https://github.com/GoodPie/buildkite-build-notifier/releases/download/{version}/BuildkiteNotifier.zip"
+    url "https://github.com/GoodPie/buildkite-build-notifier/releases/download/v#{version}/BuildkiteNotifier.zip"
     name "Buildkite Notifier"
     desc "macOS menu bar app for monitoring Buildkite builds"
     homepage "https://github.com/GoodPie/BuildkiteNotifier"
