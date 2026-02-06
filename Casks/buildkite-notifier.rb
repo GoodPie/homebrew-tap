ecbc24b3d47b03bed52bc4a8e20a206938493c2b
Brandyn Britton <brandynbb96@gmail.com>
2026-02-06 21:39:38 +0800

    bump(buildkite-notifier): Update buildkite version

--- Casks/buildkite-notifier.rb
+++ Casks/buildkite-notifier.rb
@@ -1,6 +1,6 @@
 cask "buildkite-notifier" do
-    version "0.0.4"
-    sha256 "60ed0684a1cb34cffb43b35804d762970782ed8d0736b7ad3a1c191036912198"
+    version "0.0.5"
+    sha256 "b4d9af325b97097be82de225497a5bb0758de8ee06c938a23a1879d99c18c4ee"
 
     url "https://github.com/GoodPie/buildkite-build-notifier/releases/download/v#{version}/BuildkiteNotifier.zip"
     name "Buildkite Notifier"
